f20af94274b02d0dc5514e8fff00e895129d8a67
Richard Kim <richardshkim04@gmail.com>
2022-05-04 20:12:29 -0400

    Add Assets model in db.py

--- backend/db.py
+++ backend/db.py
@@ -1,7 +1,104 @@
 from flask_sqlalchemy import SQLAlchemy
+import base64
+import boto3
+import datetime
+import io
+from io import BytesIO
+from mimetypes import guess_extension, guess_type
+import os
+from PIL import Image
+import random
+import re
+import string
 from matplotlib.style import library
 
 db = SQLAlchemy()
+
+EXTENSIONS = ["png", "gif", "jpg", "jpeg"]
+BASE_DIR = os.getcwd()
+S3_BUCKET_NAME = os.environ.get("S3_BUCKET_NAME")
+S3_BASE_URL = f"https://{S3_BUCKET_NAME}.s3.us-east-1.amazonaws.com"
+
+
+class Asset(db.Model):
+    __tablename__ = "assets"
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    base_url = db.Column(db.String, nullable=True)
+    salt = db.Column(db.String, nullable=False)
+    extension = db.Column(db.String, nullable=False)
+    width = db.Column(db.Integer, nullable=False)
+    height = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+
+    def __init__(self, **kwargs):
+        """
+        Initializes an Asset object
+        """
+        self.create(kwargs.get("image_data"))
+
+    def create(self, image_data):
+        """
+        Given an image in base64 form, does the following:
+        1. Rejects the image if it is not a support filetype
+        2. Generates a random string for the image filename
+        3. Decodes the image and attempts to upload it to AWS
+        """
+        try:
+            ext = guess_extension(guess_type(image_data)[0])[1:]
+
+            # only accept supported file extensions
+            if ext not in EXTENSIONS:
+                raise Exception(f"Unsupported file type: {ext}")
+
+            # secure way of generating a random string for image filename
+            salt = "".join(
+                random.SystemRandom().choice(
+                    string.ascii_uppercase + string.digits
+                )
+                for _ in range(16)
+            )
+
+            # remove header of base64 string
+            img_str = re.sub("^data:image/.+;base64,", "", image_data)
+            img_data = base64.b64decode(img_str)
+            img = Image.open(BytesIO(img_data))
+
+            self.base_url = S3_BASE_URL
+            self.salt = salt
+            self.extension = ext
+            self.width = img.width
+            self.height = img.height
+            self.created_at = datetime.datetime.now()
+
+            img_filename = f"{self.salt}.{self.extension}"
+            self.upload(img, img_filename)
+
+        except Exception as e:
+            print(f"Error when creating image: {e}")
+
+    def upload(self, img, img_filename):
+        """
+        Attempts to upload the image to the specified S3 bucket
+        """
+        try:
+            # save image temporarily on server
+            img_temploc = f"{BASE_DIR}/{img_filename}"
+            img.save(img_temploc)
+
+            # upload the image to S3
+            s3_client = boto3.client("s3")
+            s3_client.upload_file(img_temploc, S3_BUCKET_NAME, img_filename)
+
+            # make S# image url public
+            s3_resource = boto3.resource("s3")
+            object_acl = s3_resource.ObjectAcl(S3_BUCKET_NAME, img_filename)
+            object_acl.put(ACL="public-read")
+
+            # remove image from server
+            os.remove(img_temploc)
+        except Exception as e:
+            print(f"Error when uploading image: {e}")
+
 
 class User(db.Model):
     """
@@ -40,6 +137,7 @@ class User(db.Model):
             "password": self.password
         }
 
+
 class Library(db.Model):
     """
     Library model
@@ -55,7 +153,7 @@ class Library(db.Model):
 
     def getTimeStart(self):
         return self.time_start
-    
+
     def getTimeEnd(self):
         return self.time_end
 
@@ -102,10 +200,10 @@ class Room(db.Model):
 
     def getName(self):
         return self.name
-    
+
     def getLibrary(self):
         return Library.query.filter_by(id=self.library_id).first()
-    
+
     def getLibraryName(self):
         library = self.getLibrary()
         return library.getName()
@@ -140,6 +238,7 @@ class Room(db.Model):
             "name": self.name,
             "capacity": self.capacity,
         }
+
 
 class Timeslot(db.Model):
     """
